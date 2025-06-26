2a7a36ef96aad2cdae46f2a630167f00473ab100
meatspace <bmd103@gmail.com>
2025-06-26 13:05:06 -0800

    fix func_conveyor
    code fix found on momentum mod repo

--- sp/src/game/server/bmodels.cpp
+++ sp/src/game/server/bmodels.cpp
@@ -254,6 +254,7 @@ void CFuncVehicleClip::InputDisable( inputdata_t &data )
 
 #define SF_CONVEYOR_VISUAL		0x0001
 #define SF_CONVEYOR_NOTSOLID	0x0002
+#define SF_CONVEYOR_START_DISABLED 0x0003
 
 class CFuncConveyor : public CFuncWall
 {
@@ -285,7 +286,7 @@ LINK_ENTITY_TO_CLASS( func_conveyor, CFuncConveyor );
 BEGIN_DATADESC( CFuncConveyor )
 
 	DEFINE_INPUTFUNC( FIELD_VOID, "ToggleDirection", InputToggleDirection ),
-	DEFINE_INPUTFUNC( FIELD_VOID, "SetSpeed", InputSetSpeed ),
+	DEFINE_INPUTFUNC(FIELD_FLOAT, "SetSpeed", InputSetSpeed),
 
 	DEFINE_KEYFIELD( m_vecMoveDir, FIELD_VECTOR, "movedir" ),
 	DEFINE_FIELD( m_flConveyorSpeed, FIELD_FLOAT ),
@@ -320,8 +321,13 @@ void CFuncConveyor::Spawn( void )
 		AddSolidFlags( FSOLID_NOT_SOLID );
 	}
 
-	if ( m_flSpeed == 0 )
-		m_flSpeed = 100;
+	if (!HasSpawnFlags(SF_CONVEYOR_VISUAL))
+	{
+		if (m_flSpeed == 0)
+		{
+			m_flSpeed = 100;
+		}
+	}
 
 	UpdateSpeed( m_flSpeed );
 }
